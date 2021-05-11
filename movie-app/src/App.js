95794ece02f223136f1b328454031cecfb831f8e
MattGriff00 <72370376+MattGriff00@users.noreply.github.com>
2021-05-10 23:02:08 -0400

    v1

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,31 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../src/App.css';
+import './App.css';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
 
-
-
 const App = () => {
-  const [movies, setMovies] = useState([])
-  const [searchValue, setSearchValue] = useState('');
-    
-  const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=ddfc933`;
+	const [movies, setMovies] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
 
-    const response = await fetch(url);
-    const responseJSON = await response.json();
+	const getMovieRequest = async () => {
+		const url = `http://www.omdbapi.com/?s=star wars&apikey=263d22d8`;
 
-    if(responseJSON.Search){
-      setMovies (responseJSON.Search);
-    }
-  };
+		const response = await fetch(url);
+		const responseJson = await response.json();
+
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+	};
 
 	useEffect(() => {
-		getMovieRequest(searchValue);
-	}, [searchValue]);
-    
+		getMovieRequest();
+	}, []);
+
 	return (
 		<div className='container-fluid movie-app'>
 			<div className='row d-flex align-items-center mt-4 mb-4'>
